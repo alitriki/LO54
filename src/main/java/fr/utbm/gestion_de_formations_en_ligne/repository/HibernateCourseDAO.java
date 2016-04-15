1553151a1170121f62ef1784c416bd5eddc03b1c
Eddie <Eddie@172.17.17.185>
2016-04-15 16:35:42 +0200

    J'ai mis espace dans la classe Cleint

--- src/main/java/fr/utbm/gestion_de_formations_en_ligne/repository/HibernateCourseDAO.java
+++ src/main/java/fr/utbm/gestion_de_formations_en_ligne/repository/HibernateCourseDAO.java
@@ -5,10 +5,24 @@
  */
 package fr.utbm.gestion_de_formations_en_ligne.repository;
 
+import fr.utbm.gestion_de_formations_en_ligne.entity.Course;
+import fr.utbm.gestion_de_formations_en_ligne.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 /**
  *
  * @author Eddie
  */
 public class HibernateCourseDAO {
     
+    public List<Course> getAllCourses()
+    {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        
+        Query query = session.createQuery("from COURSE");
+        List<Course> listCourse = query.list();
+        return listCourse;
+    }
 }
