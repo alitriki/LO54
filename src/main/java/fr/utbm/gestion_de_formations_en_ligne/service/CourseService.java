712cdb994f1f1b09631d0e68881ac61ba44ec7a2
Eddie <Eddie@172.17.17.185>
2016-04-15 16:19:22 +0200

    J'ai mis espace dans la classe Cleint

---
+++ src/main/java/fr/utbm/gestion_de_formations_en_ligne/service/CourseService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.utbm.gestion_de_formations_en_ligne.service;
+
+/**
+ *
+ * @author Eddie
+ */
+public class CourseService {
+    
+}
