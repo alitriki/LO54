6dd316595e0636345595848c6f28e1672f167f1b
Eddie <Eddie@172.17.17.185>
2016-04-15 17:52:49 +0200

    HibernateCourseDao -> getAllCoursesAtDate(Date)  created HibernateCourseDao -> getAllCoursesAtLocation(Location)  created

--- src/main/java/fr/utbm/gestion_de_formations_en_ligne/repository/HibernateCourseDAO.java
+++ src/main/java/fr/utbm/gestion_de_formations_en_ligne/repository/HibernateCourseDAO.java
@@ -6,7 +6,9 @@
 package fr.utbm.gestion_de_formations_en_ligne.repository;
 
 import fr.utbm.gestion_de_formations_en_ligne.entity.Course;
+import fr.utbm.gestion_de_formations_en_ligne.entity.Location;
 import fr.utbm.gestion_de_formations_en_ligne.util.HibernateUtil;
+import java.util.Date;
 import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -21,7 +23,7 @@ public class HibernateCourseDAO {
     {
         Session session = HibernateUtil.getSessionFactory().openSession();
         
-        Query query = session.createQuery("from COURSE");
+        Query query = session.createQuery("from Course");
         List<Course> listCourse = query.list();
         return listCourse;
     }
@@ -31,10 +33,35 @@ public class HibernateCourseDAO {
         Session session = HibernateUtil.getSessionFactory().openSession();
         String keyWord = "%";
         keyWord.concat(motCle).concat("%");
-        Query query = session.createQuery("from COURSE as course where course.TITLE like :motcle");
+        Query query = session.createQuery("from Course as course where course.title like :motcle");
         query.setString(0, keyWord);
         List<Course> listCourse = query.list();
         return listCourse;
     }        
+    
+    public List<Course> getAllCoursesAtDateHibernate(Date date)
+    {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        
+        Query query = session.createQuery("from Course as course"
+                + "                        inner join CourseSession as coursesession"
+                + "                        where :date <= coursesession.startDate");
+        query.setDate(0, date);
+        List<Course> listCourse = query.list();
+        return listCourse;
+    }
+    
+    public List<Course> getAllCoursesAtLocationHibernate(Location location)
+    {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        
+        Query query = session.createQuery("from Course as course"
+                + "                        inner join CourseSession as coursesession"
+                + "                        inner join Location as location"
+                + "                        where location.city = :ville");
+        query.setString(0, location.getCity());
+        List<Course> listCourse = query.list();
+        return listCourse;
+    }
     
 }
